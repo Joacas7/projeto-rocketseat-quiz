94b8dd0a5891c9700876fc5e165b17aff7d49b2d
João Victor <kleber@gmail.com>
2024-04-08 22:35:18 -0300

    Correção no HTML e Javascript

--- function.js
+++ function.js
@@ -6,96 +6,197 @@ alert(mensagem + (10 * 100) + " abraços");
 /* todas as perguntas e alternativas */
 const perguntas = [
     {
-        pergunta: "Qual é a palavra-chave utilizada para declarar uma variável em JavaScript?",
+        pergunta: "Qual é a palavra-chave utilizada para declarar uma função em JavaScript?",
         respostas:[
-            "var",
-            "let",
-            "const"
-        ],
-        correta: 2
-    },
-    {
-        pergunta: "Qual método é utilizado para adicionar um elemento ao final de um array em JavaScript?",
-        respostas:[
-            "push()",
-            "append()",
-            "addToEnd()"
+            "function",
+            "def",
+            "fn"
         ],
         correta: 0
     },
     {
-        pergunta: "O que o operador '===' faz em JavaScript?",
+        pergunta: "Qual método é usado para remover o último elemento de um array em JavaScript?",
         respostas:[
-            "Compara apenas o valor de dois valores.",
-            "Compara o valor e o tipo de dois valores.",
-            "Realiza uma operação de atribuição."
+            "removeLast()",
+            "pop()",
+            "deleteLast()"
         ],
         correta: 1
     },
     {
-        pergunta: "Qual é o método usado para converter uma string em minúsculas em JavaScript?",
+        pergunta: "O que o método 'addEventListener()' faz em JavaScript?",
+        respostas:[
+            "Remove um evento de um elemento",
+            "Adiciona um evento a um elemento",
+            "Atualiza um elemento na página"
+        ],
+        correta: 1
+    },
+    {
+        pergunta: "Qual operador é usado para verificar se dois valores são iguais, mas não necessariamente do mesmo tipo, em JavaScript?",
+        respostas:[
+            "==",
+            "===",
+            "!="
+        ],
+        correta: 0
+    },
+    {
+        pergunta: "Qual função é usada para arredondar um número para o inteiro mais próximo em JavaScript?",
+        respostas:[
+            "round()",
+            "ceil()",
+            "floor()"
+        ],
+        correta: 0
+    },
+    {
+        pergunta: "O que é uma expressão regular em JavaScript?",
+        respostas:[
+            "Um tipo de dado primitivo",
+            "Uma estrutura de controle de fluxo",
+            "Um padrão de busca de texto"
+        ],
+        correta: 2
+    },
+    {
+        pergunta: "Qual é o resultado da expressão 'typeof null' em JavaScript?",
+        respostas:[
+            "'null'",
+            "'object'",
+            "'undefined'"
+        ],
+        correta: 1
+    },
+    {
+        pergunta: "Qual método é usado para concatenar dois ou mais arrays em JavaScript?",
+        respostas:[
+            "concat()",
+            "combine()",
+            "merge()"
+        ],
+        correta: 0
+    },
+    {
+        pergunta: "Qual é a função do operador 'new' em JavaScript?",
+        respostas:[
+            "Criar uma nova variável",
+            "Instanciar um novo objeto",
+            "Definir um novo escopo"
+        ],
+        correta: 1
+    },
+    {
+        pergunta: "Qual método é usado para transformar um array em uma string em JavaScript?",
+        respostas:[
+            "toString()",
+            "join()",
+            "concat()"
+        ],
+        correta: 1
+    },
+    {
+        pergunta: "O que o método 'splice()' faz em JavaScript?",
+        respostas:[
+            "Remove elementos de um array e/ou adiciona novos elementos",
+            "Ordena os elementos de um array",
+            "Inverte a ordem dos elementos de um array"
+        ],
+        correta: 0
+    },
+    {
+        pergunta: "Qual é a palavra-chave utilizada para declarar uma função em JavaScript?",
+        respostas:[
+            "function",
+            "def",
+            "funct"
+        ],
+        correta: 0
+    },
+    {
+        pergunta: "Como você verifica o tipo de uma variável em JavaScript?",
+        respostas:[
+            "typeof",
+            "type",
+            "instanceof"
+        ],
+        correta: 0
+    },
+    {
+        pergunta: "Qual método é usado para converter uma string em minúsculas em JavaScript?",
         respostas:[
             "toLowerCase()",
-            "toUpperCase()",
-            "convertToLower()"
+            "toLower()",
+            "lowerCase()"
         ],
         correta: 0
     },
     {
-        pergunta: "Qual é a forma correta de comentar múltiplas linhas de código em JavaScript?",
+        pergunta: "O que o método 'push()' faz em um array em JavaScript?",
         respostas:[
-            "// Comentário",
-            "/* Comentário */",
-            "--Comentário--"
-        ],
-        correta: 1
-    },
-    {
-        pergunta: "Qual dessas opções é uma maneira de selecionar um elemento HTML pelo seu ID em JavaScript?",
-        respostas:[
-            "document.getElementByName()",
-            "document.querySelectorAll()",
-            "document.getElementById()"
+            "Remove o último elemento",
+            "Adiciona um elemento no início",
+            "Adiciona um elemento no final"
         ],
         correta: 2
+    },
+    {
+        pergunta: "Qual operador é usado para atribuição de valor em JavaScript?",
+        respostas:[
+            "=",
+            "==",
+            "==="
+        ],
+        correta: 0
+    },
+    {
+        pergunta: "Como você faz um loop 'for' em JavaScript?",
+        respostas:[
+            "for (var i = 0; i < length; i++)",
+            "loop (var i = 0; i < length; i++)",
+            "foreach (var i = 0; i < length; i++)"
+        ],
+        correta: 0
     },
     {
         pergunta: "Qual método é usado para remover o último elemento de um array em JavaScript?",
         respostas:[
             "pop()",
-            "remove()",
-            "delete()"
+            "removeLast()",
+            "deleteLast()"
         ],
         correta: 0
     },
     {
         pergunta: "O que o método 'addEventListener()' faz em JavaScript?",
         respostas:[
-            "Adiciona um ouvinte de evento a um elemento HTML.",
-            "Remove um ouvinte de evento de um elemento HTML.",
-            "Atualiza um ouvinte de evento em um elemento HTML."
+            "Adiciona uma classe a um elemento HTML",
+            "Registra um manipulador de eventos em um elemento HTML",
+            "Remove um evento de um elemento HTML"
+        ],
+        correta: 1
+    },
+    {
+        pergunta: "Como você declara uma constante em JavaScript?",
+        respostas:[
+            "var",
+            "const",
+            "let"
+        ],
+        correta: 1
+    },
+    {
+        pergunta: "Qual é o operador lógico 'OU' em JavaScript?",
+        respostas:[
+            "||",
+            "&&",
+            "!"
         ],
         correta: 0
-    },
-    {
-        pergunta: "Qual é o operador usado para comparar se dois valores são diferentes em JavaScript?",
-        respostas:[
-            "!=",
-            "!==",
-            "=="
-        ],
-        correta: 1
-    },
-    {
-        pergunta: "Qual é o resultado da expressão '3 + '3' em JavaScript?",
-        respostas:[
-            "6",
-            "'33'",
-            "33"
-        ],
-        correta: 1
     }
+
 ];
+
 
 /* transforma o html visivel atraves do JavaScript sem precisar escrever todo o html */
 const quiz = document.querySelector("#quiz");
